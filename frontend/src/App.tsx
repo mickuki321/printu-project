c4da8448dfcc43ee3b2be3b5f817f10789559f5e
mickuki321 <michalkubiak2022@gmail.com>
2023-05-29 20:25:29 +0200

    chore: create react app and config TS, prettier and eslint

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+const App = (): JSX.Element => (
+	<div className='App'>
+		<header className='App-header'>
+			<a
+				className='App-link'
+				href='https://reactjs.org'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+                test
+			</a>
+		</header>
+	</div>
+);
+
+export default App;
